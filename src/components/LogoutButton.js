cc21a77a67722a8ec8e0a9f27a2fce3fae393d1a
aselwilliams <aselwilliams22@gmail.com>
2022-09-01 00:15:41 -0500

    loading added

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -2,11 +2,13 @@ import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 
 function LogoutButton() {
-    const {logout} = useAuth0()
+    const {logout, isAuthenticated} = useAuth0()
   return (
-   <button onClick={()=>logout()}>
-       Log Out
-   </button>
+      isAuthenticated && (
+    <button onClick={()=>logout()}>
+        Log Out
+    </button>
+      )
   )
 }
 
