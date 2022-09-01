3cdf5d90cc145a176d0af7aeadb699885be550e0
aselwilliams <aselwilliams22@gmail.com>
2022-09-01 00:17:16 -0500

    formatted the code

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,15 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
 
 function LogoutButton() {
-    const {logout, isAuthenticated} = useAuth0()
-  return (
-      isAuthenticated && (
-    <button onClick={()=>logout()}>
-        Log Out
-    </button>
-      )
-  )
+  const { logout, isAuthenticated } = useAuth0();
+  return isAuthenticated && <button onClick={() => logout()}>Log Out</button>;
 }
 
-export default LogoutButton
+export default LogoutButton;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,20 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
-import JSONPretty from 'react-json-pretty'
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import JSONPretty from "react-json-pretty";
 
 function Profile() {
-    const {user, isAuthenticated}=useAuth0()
+  const { user, isAuthenticated } = useAuth0();
   return (
-      isAuthenticated && (
-    <div>
+    isAuthenticated && (
+      <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
         <JSONPretty data={user} />
         {JSON.stringify(user, null, 2)}
-    </div>
-      )
-    
-  )
+      </div>
+    )
+  );
 }
 
-export default Profile
+export default Profile;
