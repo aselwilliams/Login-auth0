b3930a27bf28623d6265a8d7d55ade4589fd8c06
aselwilliams <aselwilliams22@gmail.com>
2022-09-01 00:08:39 -0500

    user info is rendered

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,20 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
+import JSONPretty from 'react-json-pretty'
 
 function Profile() {
-    const user=useAuth0()
+    const {user, isAuthenticated}=useAuth0()
   return (
+      isAuthenticated && (
     <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <JSONPretty data={user} />
         {JSON.stringify(user, null, 2)}
     </div>
+      )
+    
   )
 }
 
