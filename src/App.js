9017368a07cc71b874dea909f1318d30310d0be4
aselwilliams <aselwilliams22@gmail.com>
2022-08-31 23:54:30 -0500

    Logout btn works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
+    <>
    <LoginButton />
+   <LogoutButton />
+   <Profile />
+   </>
   );
 }
 
